574cc6deba0d11f35c36293279e206caad7c6699
tugba-koc <koctug@itu.edu.tr>
2024-03-19 15:55:55 +0300

    feat: add bcrypted password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model, models } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Schema(
   {
@@ -20,7 +21,7 @@ const UserSchema = new Schema(
       required: true,
       minlength: 5,
       maxlength: 1024,
-      validate: {
+      /*       validate: {
         validator: function (v) {
           return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/.test(
             v
@@ -28,10 +29,16 @@ const UserSchema = new Schema(
         },
         message: (props) =>
           `${props.value} is not a valid password! Passwords must be at least 5 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.`,
-      },
+      }, */
     },
   },
   { timestamps: true }
 );
+
+UserSchema.post('validate', async (user) => {
+  const unHashedPassword = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(unHashedPassword, salt);
+});
 
 export const User = models?.User || model('User', UserSchema);
