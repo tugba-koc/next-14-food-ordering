0eeffad2baa2754d90708a6fc6bb7e066dfc9d40
tugba-koc <koctug@itu.edu.tr>
2024-03-19 15:56:09 +0300

    fix: compile bug

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,9 +1,9 @@
-import { User } from '@/app/models/User';
+import { User } from '../../models/User.js';
 import mongoose from 'mongoose';
 
 export async function POST(req, res) {
-  const body = req.json();
-  mongoose.connect(process.env.MONGO_URL);
+  const body = await req.json();
+  await mongoose.connect(process.env.MONGO_URL);
   const createdUser = await User.create(body);
   return Response.json({ message: createdUser });
 }
