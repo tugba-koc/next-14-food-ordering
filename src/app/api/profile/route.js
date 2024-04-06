545cc40c0bfd3a1e50505cd5b807325b44cd8d52
tugba-koc <koctug@itu.edu.tr>
2024-04-06 23:54:14 +0300

    feat: Update profile route to return session in response

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -6,6 +6,5 @@ export async function PUT(req, res) {
   await mongoose.connect(process.env.MONGO_URL);
   const body = await req.json();
   const session = await getServerSession(authOptions);
-  console.log('session > ', session);
-  return Response.json({ message: 'Profile updated' });
+  return Response.json({ session });
 }
